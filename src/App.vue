1fdaa0635599da58e7739cedb518f977f1867f15
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2020-03-25 15:40:44 -0400

    updated route

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
+
+import Navbar from "@/components/Navbar";
 export default {
-  name: 'App'
+  name: "App",
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
